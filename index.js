a7a0093a6cf1fc53079ebf797471625e6e84e793
immetras <eranusieq@gmail.com>
2023-12-01 22:50:07 +0100

    added card generation with randomized positions

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// just adding event listeners 
 function main() {
+    console.log("loaded")
     const text = document.getElementById("text");
     const s30 = document.getElementById("30s");
     const s60 = document.getElementById("60s");
@@ -18,8 +20,48 @@ function main() {
     });
 };
 
+// main game functions
 function game(time) {
+    console.log("game start")
     const initial = document.getElementById("initial");
+    const game = document.getElementById("game");
+
+    initial.style.display = "none";
+    game.style.display = "flex";
 
 
+    // generating cards in size=4x4 grid in random positions
+    let size = 4;
+
+    let cardImg = []
+    for (let i = 0; i < size * size; i++) {
+        cardImg.splice(Math.random() * i, 0, Math.ceil((i + 1) / 2))
+    };
+    console.log(cardImg)
+
+    let cards = [];
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+
+            div.className = "card";
+            div.id = `pos-x:${i},pos-y:${j}`;
+            div.style.width = 400 / size + "px";
+            div.style.height = 400 / size + "px";
+            div.addEventListener("click", cardClick)
+
+            cards.push({
+                "pos-x": i,
+                "pos-x": j,
+                "image": cardImg[(i * 4) + j]
+            });
+            game.appendChild(div);
+        };
+    };
+    console.table(cards);
+
+    // click card function
+    function cardClick() {
+        console.log(this.id)
+    };
 };
